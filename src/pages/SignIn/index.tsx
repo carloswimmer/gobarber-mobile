30624ab0fe6ecc3adfcd4cb79f0a11e92fa6f47e
Carlos Wimmer <coiote777@gmail.com>
2020-08-02 01:00:35 -0300

    Complete Auth Screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,25 +1,93 @@
-import React from 'react'
-import { Image } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react'
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+  Keyboard
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather'
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText
+} from './styles';
 
 const SignIn: React.FC = () => {
+  const [showCreateAccount, setShowCreateAccount] = useState(true)
+
+  useEffect(() => {
+    Keyboard.addListener('keyboardDidShow', _hideCreateAccount);
+    Keyboard.addListener('keyboardDidHide', _showCreateAccount);
+
+    return () => {
+      Keyboard.removeListener('keyboardDidShow', _hideCreateAccount);
+      Keyboard.removeListener('keyboardDidHide', _showCreateAccount);
+    }
+  }, [])
+
+  const _hideCreateAccount = useCallback(() => {
+    setShowCreateAccount(false);
+  }, []);
+
+  const _showCreateAccount = useCallback(() => {
+    setShowCreateAccount(true);
+  }, []);
+
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ minHeight: 100 }}
+        >
+          <Container>
+            <Image source={logoImg} />
 
-      <Title>Faça seu logon</Title>
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail"/>
-      <Input name="password" icon="lock" placeholder="Senha"/>
+            <Input name="email" icon="mail" placeholder="E-mail"/>
+            <Input name="password" icon="lock" placeholder="Senha"/>
 
-      <Button onPress={() => {console.log('ok')}}>Entrar</Button>
-    </Container>
+            <Button onPress={() => {console.log('ok')}}>Entrar</Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>
+                Esqueci minha senha
+              </ForgotPasswordText>
+            </ForgotPassword>
+
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {showCreateAccount && (
+        <CreateAccountButton onPress={() => {}}>
+          <Icon name="log-in" size={20} color="#ff9000"/>
+
+          <CreateAccountButtonText>
+            Criar uma conta
+          </CreateAccountButtonText>
+
+        </CreateAccountButton>
+      )}
+    </>
   );
 }
 
