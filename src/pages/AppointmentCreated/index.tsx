c160cce155432917083c5dad1ace5fd792f3356d
Carlos Wimmer <coiote777@gmail.com>
2020-10-20 12:38:43 -0300

    Create appointment created screen

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
-import { Container, Title } from './styles';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-import { useAuth } from '../../hooks/auth';
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
 
-import Button from '../../components/Button';
+interface RouteParams {
+  date: number;
+}
 
 const AppointmentCreated: React.FC = () => {
-  const { signOut } = useAuth();
+  const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBR },
+    );
+  }, [routeParams.date]);
 
   return (
     <Container>
-      <Title>GoBarber AppointmentCreated</Title>
-      <Button onPress={signOut}>Sair</Button>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>{formattedDate}</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
     </Container>
   );
 };
--- src/pages/AppointmentCreated/styles.ts
+++ src/pages/AppointmentCreated/styles.ts
@@ -1,15 +1,40 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  padding: 0 30px;
+  padding: 0 24px;
 `;
 
 export const Title = styled.Text`
-  font-size: 24px;
+  font-size: 32px;
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
-  margin: 0 0 24px 0;
+  margin: 48px 0 0;
+  text-align: center;
+`;
+
+export const Description = styled.Text`
+  font-size: 18px;
+  color: #999591;
+  font-family: 'RobotoSlab-Regular';
+  margin: 16px 0 0;
+  text-align: center;
+`;
+
+export const OkButton = styled(RectButton)`
+  background: #ff9000;
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  margin: 24px;
+  padding: 12px 24px;
+`;
+
+export const OkButtonText = styled.Text`
+  font-size: 18px;
+  color: #312e38;
+  font-family: 'RobotoSlab-Medium';
 `;
